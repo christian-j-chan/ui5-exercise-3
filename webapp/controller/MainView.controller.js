f23055877fa6512136c3fd0177589f84e76b7095
Christian jerald Chan <cxtronica.work@gmail.com>
2025-05-26 03:10:18 +0000

    more fragment commits

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -14,9 +14,23 @@ sap.ui.define([
         },
 
         onAddItem: function (){
-            var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+            /*var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
             var sMsg = oTextBundle.getText("addButtonMsg");
-            this.fnDisplayMsg(sMsg);
+            this.fnDisplayMsg(sMsg);*/
+
+             // Instantiate the fragment
+
+            // create dialog lazily
+            if (!this.oDialog) {
+                // By using loadFragment, we are adding the fragment as a dependent to the View
+                // By doing so, we can use the functions inside the view's controller
+                this.oDialog = this.loadFragment({
+                    name: "com.training.exer3chan.fragment.ProductDialog"
+                });
+            } 
+            this.oDialog.then(function(oDialog) {
+                oDialog.open();
+            });
         },
 
         onChangeMOP: function (oEvent) {
@@ -70,6 +84,11 @@ sap.ui.define([
                 sap.m.MessageToast.show("Required Field is blank"); 
             } 
         },
+
+        onCloseDialog: function (){
+            this.getView().byId("idProductDialog").close();
+        },
+
 
     });
 });
